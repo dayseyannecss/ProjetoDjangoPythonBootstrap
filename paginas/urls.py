c73c277d696377a0e7cd471be3434717db8ec6f6
dayse.sousa@ifma.edu.br <dayse.sousa@ifma.edu.br>
2022-11-03 12:41:27 -0300

    Loja de Vendas - Inserido template Bootstrap

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import Index , Modelo
 
 urlpatterns = [
     path('', Index.as_view(), name ='inicio'),
+    path('modelo', Modelo.as_view() , name ='modelo'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 class Index(TemplateView):
 
     template_name = 'index.html'
+
+class Modelo(TemplateView):
+
+    template_name = 'modelo.html'
